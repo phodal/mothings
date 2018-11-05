5cf50ccc04ccd4545ca7a26ce47834064f6ab67e
Phodal HUANG <h@phodal.com>
2018-11-05 08:56:48 +0800

    [pass] update display ways

--- mopass.py
+++ mopass.py
@@ -3,7 +3,10 @@ from string import hexdigits, punctuation
 import time
 
 base_time = str(time.time())[12:16]
+print(time.time())
 base_string = ''.join(choice(hexdigits) for i in range(6))
+print(hexdigits)
 base_punctuation = ''.join(choice(punctuation) for j in range(2))
+print(punctuation)
 
 print(base_string + base_punctuation + base_time)
